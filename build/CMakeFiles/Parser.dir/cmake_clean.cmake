a1c6a749286ad759c6600374dbcb77dcd8ffc972
Marion Hoogstoel <marion.hoogstoel@gmail.com>
2011-08-09 16:48:28 -0400

    allez allez

---
+++ build/CMakeFiles/Parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Parser.dir/main.cpp.o"
+  "parser_clpCLP.h"
+  "Parser.pdb"
+  "Parser"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
